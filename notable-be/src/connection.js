56701570f8635b77b502e458bf214010d5e6af8f
Kaelin Newhouse <kaelinnewhouse@Kaelins-MBP.attlocal.net>
2020-11-10 12:56:26 -0500

    Changed personal db info

--- notable-be/src/connection.js
+++ notable-be/src/connection.js
@@ -4,7 +4,7 @@ let mysql = require('mysql2');
 let pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "student",
+  password: "password",
   database: "notable",
   connectionLimit: 10,
   queueLimit: 0
