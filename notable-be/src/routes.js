abeece90de4b8e2c33966dccb53a6ec0245769a6
hg01195 <hg01195@georgiasouthern.edu>
2020-11-12 23:01:13 -0500

    API to update notes

--- notable-be/src/routes.js
+++ notable-be/src/routes.js
@@ -53,9 +53,8 @@ router.get('/notes', function (req, res) {
     pool.getConnection(function (err, conn) {
         if (err) { res.status(400).json("Could not connect to database, check server"); }
         else {
-            responseObject = {}
             conn.query('SELECT * FROM data LEFT JOIN note ON data.dataid = note.dataref LEFT JOIN image ON image.data = data.dataid WHERE data.user = ?', req.query.userid, function (err, userNotes, fields) {
-                if (err) { { res.status(404).json(responseObject); } }
+                if (err) { { res.status(404).json(err.message); } }
                 else { res.status(200).json(userNotes); }
             })
             pool.releaseConnection(conn);
@@ -64,7 +63,7 @@ router.get('/notes', function (req, res) {
 })
 
 
-//create note
+//create note API
 router.post('/create', function (req, res) {
     pool.getConnection(function (err, conn) {
         if (err) { res.status(400).json("Could not connect to database, check server"); }
@@ -92,6 +91,27 @@ router.post('/create', function (req, res) {
                     pool.releaseConnection(conn);
             });
 
+        }
+    })
+})
+
+//update note API
+router.post('/update', function (req, res) {
+    pool.getConnection(function (err, conn) {
+        if (err) { res.status(400).json("Could not connect to database, check server"); }
+        else {
+            conn.query('UPDATE note SET text = ? WHERE noteid = ?', [req.body.text, req.body.noteid], function (err, update, fields) {
+                if (err) { res.status(404).json(err.message);  }
+                else { 
+                    conn.query('SELECT * FROM note WHERE noteid = ?', req.body.noteid, function (err2, updatedNote){
+                        if (err2){ res.status(404).json(err.message); }
+                        else {
+                            res.status(200).json(updatedNote); }
+                    })
+                    }
+
+            })
+            pool.releaseConnection(conn);
         }
     })
 })
