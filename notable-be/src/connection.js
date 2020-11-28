7cc5c3d0f19096698657c7beab6dbf8006a9c544
Toyin <raji.toyin12@gmail.com>
2020-11-28 08:23:38 -0500

    keeping database password same as mine

--- notable-be/src/connection.js
+++ notable-be/src/connection.js
@@ -4,11 +4,7 @@ let mysql = require('mysql2');
 let pool = mysql.createPool({
   host: "localhost",
   user: "root",
-<<<<<<< HEAD
   password: "Computertechie2020",
-=======
-  password: "password",
->>>>>>> 65cd404720ab195e1a507200e3dd75e6fa161d3b
   database: "notable",
   connectionLimit: 10,
   queueLimit: 0
